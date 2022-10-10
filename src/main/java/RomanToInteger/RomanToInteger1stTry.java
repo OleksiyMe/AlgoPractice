37dd43736be5e6058b028c127900ee543c901135
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-10-09 20:50:21 -0400

    Roman To Integer 2 attempts

---
+++ src/main/java/RomanToInteger/RomanToInteger1stTry.java
@@ -0,0 +1,52 @@
+package RomanToInteger;
+
+import java.util.*;
+
+import static java.util.Map.entry;
+
+public class RomanToInteger1stTry {
+
+    public static void main(String[] args) {
+
+        String input = "MCMXCIV";
+
+        System.out.println(romanToInteger(input));
+
+
+    }
+
+    private static int romanToInteger(String s) {
+        HashMap<String, Integer> dic = new HashMap<>();
+                dic.put("I", 1);
+        dic.put("IV", 4);
+        dic.put("V", 5);
+        dic.put("IX", 9);
+        dic.put("X", 10);
+        dic.put("XL", 40);
+        dic.put("L", 50);
+        dic.put("XC", 90);
+        dic.put("C", 100);
+        dic.put("CD", 400);
+        dic.put("D", 500);
+        dic.put("CM", 900);
+        dic.put("M", 1000);
+
+
+        int result = 0;
+        String[] array = s.split("");
+        int lastIndex = array.length - 1;
+        for (int i = 0; i <= lastIndex; i++) {
+
+            if (i != lastIndex && dic.get(array[i]) < dic.get(array[i + 1])) {
+                result += dic.get(array[i] + array[i + 1]);
+                i++;
+            } else result += dic.get(array[i]);
+
+
+        }
+
+
+        return result;
+    }
+
+}
---
+++ src/main/java/RomanToInteger/RomanToIntegerSecondTry.java
@@ -0,0 +1,48 @@
+package RomanToInteger;
+
+import java.util.HashMap;
+
+public class RomanToIntegerSecondTry {
+    public static void main(String[] args) {
+
+        String input = "MCMXCIV";
+
+        System.out.println(romanToInteger(input));
+
+
+    }
+
+    private static int romanToInteger(String s) {
+        HashMap<String, Integer> dic = new HashMap<>();
+        dic.put("I", 1);
+        dic.put("IV", 4);
+        dic.put("V", 5);
+        dic.put("IX", 9);
+        dic.put("X", 10);
+        dic.put("XL", 40);
+        dic.put("L", 50);
+        dic.put("XC", 90);
+        dic.put("C", 100);
+        dic.put("CD", 400);
+        dic.put("D", 500);
+        dic.put("CM", 900);
+        dic.put("M", 1000);
+
+
+        int result = 0;
+        int lastIndex = s.length() - 1;
+        for (int i = 0; i <= lastIndex; i++) {
+
+            if (i != lastIndex &&
+                    dic.get(s.substring(i, i + 1)) <
+                            dic.get(s.substring((i + 1), i + 2))) {
+                result += dic.get(s.substring(i, i + 1) + s.substring((i + 1), i + 2));
+                i++;
+            }else
+                result += dic.get(s.substring(i, i + 1));
+        }
+        return result;
+
+
+    }
+}
