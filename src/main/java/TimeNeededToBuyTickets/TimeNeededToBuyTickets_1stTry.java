985a73e464551507ed733fcda3dafa8544c91eb2
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-10-25 22:42:53 -0400

    TimeNeededToBuyTickets_1stTry

---
+++ src/main/java/TimeNeededToBuyTickets/TimeNeededToBuyTickets_1stTry.java
@@ -0,0 +1,29 @@
+package TimeNeededToBuyTickets;
+
+public class TimeNeededToBuyTickets_1stTry {
+
+    public static void main(String[] args) {
+
+        int[] tickets = {2,3,2};   //queue of people And their demand for tickets
+        int k = 2; // index of person for which we will find time taken to buy tickets
+
+        System.out.println("It takes: " + timeRequiredToBuy(tickets, k) + " seconds.");
+
+
+    }
+
+    private static int timeRequiredToBuy(int[] tickets, int k) {
+        int result = 0;
+        for (int i = 0; tickets[k] > 0; i++) {
+            if (i == tickets.length) i = 0;
+            if (tickets[i] > 0) {
+                tickets[i] = tickets[i] - 1;
+                result++;
+
+            }
+        }
+        return result;
+    }
+
+
+}
