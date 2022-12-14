7eb24175da5f7217ba881ca5b27785260142fbfb
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-12-14 00:11:06 -0500

    some algo

--- src/main/java/FrequencyOfWords/_2ndTryWithRegex.java
+++ src/main/java/FrequencyOfWords/_2ndTryWithRegex.java
@@ -4,20 +4,20 @@ public class _2ndTryWithRegex {
 
     public static void main(String[] args) {
 
-        String input = "“This” is an example!     of Example… abc";
+        String input = " “This” is an example!     of Example… abc";
         System.out.println(freqOfWords(input));
 
     }
 
     private static String freqOfWords(String input) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder("{");
         //leave only letters and spaces
         input = input.toLowerCase().replaceAll("[^a-zA-z ]", "");
         //remove duplicated spaces
         input = input.replaceAll(" +", " ");
 
 
-        for (String each : input.split(" ")) {
+        for (String each : input.trim().split(" ")) {
             //count repeating current word in input
             int count = (input.replace(each, each + 1).length() - input.length());
             //if we already counted word -- continue
