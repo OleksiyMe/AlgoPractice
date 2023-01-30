56c0a73adc544886d530dde14ed436ce1a2efc0c
Oleksiy Melnychuk <melnichuk@gmail.com>
2023-01-30 17:01:07 -0500

    max num of nonrepeating symbols

--- src/main/java/LongestSubstringWithNonRepeatingCh/_3rd_try_29Jan23.java
+++ src/main/java/LongestSubstringWithNonRepeatingCh/_3rd_try_29Jan23.java
@@ -1,21 +1,20 @@
-package a123;
+package LongestSubstringWithNonRepeatingCh;
 
 import java.util.HashSet;
 
-public class MaxCountOfNonRepeating {
+public class _3rd_try_29Jan23 {
 
     public static void main(String[] args) {
 
-        String str= "aabcdcaaabcdedcaaabc";
+        String s = "aabcdcaaabcdedcaaabc";
 
-        System.out.println("Count of nonrepeating is "+countNonRep(str));
+        System.out.println("The longest non repeating  part of \"" + s + "\"\n is: " + lengthOfLongestSubstring(s));
     }
 
-    private static int countNonRep(String s) {
+    public static int lengthOfLongestSubstring(String s) {
         HashSet<Character> set =new HashSet<>();
         int max=0, count=0, start=0;
         for (int i = 0; i < s.length(); i++) {
-            char ch=s.charAt(i);
             if(set.add(s.charAt(i))){
                 count++;
             } else {
@@ -27,6 +26,5 @@ public class MaxCountOfNonRepeating {
             }
         }
         return Math.max(count,max);
-
     }
 }
--- src/main/java/LongestSubstringWithNonRepeatingCh/_4th_try_29Jan23.java
+++ src/main/java/LongestSubstringWithNonRepeatingCh/_4th_try_29Jan23.java
@@ -1,21 +1,20 @@
-package a123;
+package LongestSubstringWithNonRepeatingCh;
 
 import java.util.HashSet;
 
-public class MaxCountOfNonRepeating {
+public class _4th_try_29Jan23 {
 
     public static void main(String[] args) {
 
-        String str= "aabcdcaaabcdedcaaabc";
+        String s = "aabcdcaaabcdedcaaabc";
 
-        System.out.println("Count of nonrepeating is "+countNonRep(str));
+        System.out.println("The longest non repeating  part of \"" + s + "\"\n is: " + lengthOfLongestSubstring(s));
     }
 
-    private static int countNonRep(String s) {
+    public static int lengthOfLongestSubstring(String s) {
         HashSet<Character> set =new HashSet<>();
         int max=0, count=0, start=0;
         for (int i = 0; i < s.length(); i++) {
-            char ch=s.charAt(i);
             if(set.add(s.charAt(i))){
                 count++;
             } else {
@@ -23,10 +22,9 @@ public class MaxCountOfNonRepeating {
                 count=0;
                 start++;
                 i=start-1;
-                set=new HashSet<>();
+                set.clear();
             }
         }
         return Math.max(count,max);
-
     }
 }
