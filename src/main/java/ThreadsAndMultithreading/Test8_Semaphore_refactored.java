d1d585bc5a57f39c245acdfeadacc4bf614f0ade
Oleksiy Melnychuk <melnichuk@gmail.com>
2023-03-05 15:22:09 -0500

    some stuff

--- src/main/java/ThreadsAndMultithreading/Test8_Semaphore_refactored.java
+++ src/main/java/ThreadsAndMultithreading/Test8_Semaphore_refactored.java
@@ -2,49 +2,58 @@ package ThreadsAndMultithreading;
 
 import java.util.concurrent.Semaphore;
 
-public class Test8_Semaphore {
+public class Test8_Semaphore_refactored {
+
     private volatile int counter;
     private final int counterMax;
     private Semaphore semaphoreOdd, semaphoreEven;
-    public Test8_Semaphore(int counterMax) {
+
+    public Test8_Semaphore_refactored(int counterMax) {
         this.counter = 0;
         this.counterMax = counterMax;
         this.semaphoreOdd = new Semaphore(1);
         this.semaphoreEven = new Semaphore(0);
         new Thread(()->printOdd()).start();
+        new Thread(()->printOdd()).start();
+        new Thread(()->printOdd()).start();
+        new Thread(()->printEven()).start();
+        new Thread(()->printOdd()).start();
         new Thread(()->printEven()).start();
         new Thread(()->printOdd()).start();
         new Thread(()->printEven()).start();
         new Thread(()->printOdd()).start();
         new Thread(()->printEven()).start();
+        new Thread(()->printOdd()).start();
+        new Thread(()->printOdd()).start();
+        new Thread(()->printOdd()).start();
+        new Thread(()->printOdd()).start();
+        new Thread(()->printOdd()).start();
+        new Thread(()->printOdd()).start();
     }
+
     public void printOdd() {
-        while (true) {
-            try {
-                semaphoreOdd.acquire();
-                if (counter>counterMax) return;
-                System.out.println(counter++ +"   printed by "+Thread.currentThread().getName());
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }finally {
-                semaphoreEven.release();
-            }
-        }
+        doThisPrint(semaphoreOdd, semaphoreEven);
     }
     public void printEven() {
+        doThisPrint(semaphoreEven, semaphoreOdd);
+    }
+    private void doThisPrint(Semaphore activeSemaphore, Semaphore blockedSemaphore) {
         while (true) {
             try {
-                semaphoreEven.acquire();
+                activeSemaphore.acquire();
                 if (counter>counterMax) return;
                 System.out.println(counter++ +"   printed by "+Thread.currentThread().getName());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }finally {
-                semaphoreOdd.release();
+                blockedSemaphore.release();
             }
         }
     }
+
+
+
     public static void main(String[] args) {
-        new Test8_Semaphore(100);
+        new Test8_Semaphore_refactored(100);
     }
 }
