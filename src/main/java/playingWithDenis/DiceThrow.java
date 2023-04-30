3c77a144797b95244507793bab15be0da2d2a458
Oleksiy Melnychuk <melnichuk@gmail.com>
2023-04-30 12:22:40 -0400

    play with Denis

--- src/main/java/playingWithDenis/DiceThrow.java
+++ src/main/java/playingWithDenis/DiceThrow.java
@@ -1,39 +1,49 @@
 package playingWithDenis;
 
-import java.security.SecureRandom;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class DiceThrow {
-    static volatile int sumOfTries = 0, minThrows = Integer.MAX_VALUE, maxTrrows = Integer.MIN_VALUE;
+    static volatile int sumOfTries = 0, minThrows = Integer.MAX_VALUE, maxThrows = Integer.MIN_VALUE;
 
 
-    public static int[] throwDices(int numberOfDice) {
-        int[] result = new int[numberOfDice+1];
+    //desiredNumber : 0 -- any; 1-6 -- just one side of the cube
+    // output -- array with cube sides, first element of array 1 if matching, 0 if no
+    public static int[] throwDice(int numberOfDice, int desiredNumber) {
+        int[] result = new int[numberOfDice + 1];
         boolean allTheSame = true;
+        //    if (desiredNumber!=0)
+
         Random secureRandom = new Random();
 
         for (int i = 1; i <= numberOfDice; i++) {
             result[i] = secureRandom.nextInt(6) + 1;
             if (i != 1) allTheSame &= result[i - 1] == result[i];
+            else allTheSame &= result[1] == ((desiredNumber==0)? result[1]:desiredNumber);
         }
-        if(allTheSame) result[0]=1;else result[0]=0;
+        if (allTheSame) result[0] = 1;
+        else result[0] = 0;
         return result;
+    }
+
+    public static int[] throwDice(int numberOfDice) {
+        return throwDice(numberOfDice, 0);
     }
 
     public static void main(String... privet) throws InterruptedException {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("How much dice will you throw?");
+        System.out.println("How many dice will you throw?");
         int numberOfDice = sc.nextInt();
-        System.out.println("How much attempts of getting similar dice do you want to do?");
+        System.out.println("How many attempts of getting similar dice do you want to do?");
         int numberOfAttempts = sc.nextInt();
+        System.out.println("Your desired number (1-6, 0 - for any)");
+        int desiredNumber = sc.nextInt();
         System.out.println("Calculating...");
-        ExecutorService executorService = Executors.newFixedThreadPool(200);
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
         LocalTime start = LocalTime.now();
 
 
@@ -41,15 +51,16 @@ public class DiceThrow {
             //  System.out.println("Attempt number " + j);
             Runnable toRun = () -> {
                 for (int i = 0; true; i++) {
-                    int [] result = throwDices(numberOfDice);
-                    if (result[0]==1) {
-                        // System.out.println("\t\t" + (1 + i) + " throws and we get: " + Arrays.toString(map.get(true)));
+                    int[] result = throwDice(numberOfDice, desiredNumber);
+                    if (result[0] == 1) {
+                        System.out.println("\t\t" + (1 + i) + " throws and we get: " + Arrays.toString(result));
                         synchronized (DiceThrow.class) {
                             sumOfTries += i + 1;
                             minThrows = Math.min(minThrows, i + 1);
-                            maxTrrows = Math.max(maxTrrows, i + 1);
-                            System.out.print(".");
+                            maxThrows = Math.max(maxThrows, i + 1);
                         }
+                        //System.out.print(".");
+
                         break;
                     }
                 }
@@ -70,10 +81,10 @@ public class DiceThrow {
         System.out.println();
         System.out.println("As for " + numberOfAttempts + " attempts we needed to throw "
                 + numberOfDice + " dice " +
-                sumOfTries / numberOfAttempts + " on average times to get all of them the same.");
+                sumOfTries / numberOfAttempts + " times on average to get all of them the same.");
         System.out.println("Min throws " + minThrows);
-        System.out.println("Max throws " + maxTrrows);
-        System.out.println("It took "+secondsSpent+ " seconds.");
+        System.out.println("Max throws " + maxThrows);
+        System.out.println("It took " + secondsSpent + " seconds.");
 
     }
 
