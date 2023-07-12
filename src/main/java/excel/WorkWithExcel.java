e0f28b20eae9084f6f7637877359d103a0714d29
Oleksiy Melnychuk <melnichuk@gmail.com>
2023-07-12 12:30:47 -0400

    excel

--- src/main/java/excel/WorkWithExcel.java
+++ src/main/java/excel/WorkWithExcel.java
@@ -19,9 +19,11 @@ public class WorkWithExcel {
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
-       // FileInputStream ExcelFile = new FileInputStream("src/main/java/excel/Book1.xlsx");
+       //this part works with .xlsx
+
+        // Better use OPCPackage of File FileInputStream ExcelFile = new FileInputStream("src/main/java/excel/Book1.xlsx");
         OPCPackage pkg = OPCPackage.open(new File("src/main/java/excel/Book1.xlsx"));
-        //also works like XSSFWorkbook excelWBook = new XSSFWorkbook(new File("src/main/java/excel/Book1.xlsx"));
+        //Simple way -- XSSFWorkbook excelWBook = new XSSFWorkbook(new File("src/main/java/excel/Book1.xlsx"));
         XSSFWorkbook excelWBook = new XSSFWorkbook(pkg);
         XSSFSheet excelWSheet =  excelWBook.getSheet("Sheet1");
         XSSFCell cell = excelWSheet.getRow(0).getCell(0);
@@ -29,15 +31,12 @@ public class WorkWithExcel {
         excelWBook.close();
         System.out.println(cell.toString());
 
-
-        //        Also, if you want code that doesn't care whether it's an .xls or an .xlsx:
+        //        if you want code that doesn't care whether it's an .xls or an .xlsx:
         //        Workbook wb = WorkbookFactory.create(new File("MyExcel.xls"));
         Workbook excelWBook1 = WorkbookFactory.create(new File("src/main/java/excel/Book1.xls"));
         Sheet excelWSheet1 =  excelWBook1.getSheet("Sheet1");
         Cell cell1 =excelWSheet1.getRow(0).getCell(0);
         System.out.println(cell1.toString());
         excelWBook1.close();
-
-
     }
 }
